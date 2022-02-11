e5f48d5ff615d8f65ebc54b7e02f207916f2b3bc
ivaan1992 <ivaan.lg.92@gmail.com>
2022-02-11 17:46:11 -0600

    Add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+import Cards from './components/Cards/Cards';
+import Filters from './components/Filters/Filters';
 
 const App = () => (
   <div className="App">
@@ -7,6 +9,22 @@ const App = () => (
       Rick & Morty
       <span className="text-primary"> WiKi</span>
     </h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <Filters />
+        </div>
+        <div className="col-8">
+          <div className="row">
+            <Cards />
+            <Cards />
+            <Cards />
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   </div>
 );
 export default App;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cards = () => (
+  <div className="col-4">
+    Cards
+  </div>
+);
+
+export default Cards;
